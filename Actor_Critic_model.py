71edee224ce6d87afcbdbca781460167fba54259
Kyle1993 <344927747@qq.com>
2018-04-10 14:23:53 +0800

    fix

--- Actor_Critic_model.py
+++ Actor_Critic_model.py
@@ -7,33 +7,6 @@ from torch.autograd import Variable
 from torch import optim
 from Memory import Memory
 
-#
-# class Memory():
-#     def __init__(self,max_len,batch_size):
-#         self.max_len = max_len
-#         self.current_index = -1
-#         self.counter = 0
-#         self.memory = [None for _ in range(self.max_len)]
-#         self.batch_size = batch_size
-#
-#     def append(self, state, prob, action, next_state, reward, done):
-#         self.current_index = (self.current_index + 1) % self.max_len
-#         self.memory[self.current_index] = (state, prob, action, next_state, reward, done)
-#         self.counter += 1
-#
-#     def sample(self):
-#         batch = random.sample(self.memory[:min(self.counter, self.max_len)], self.batch_size)
-#         batch = list(zip(*batch))
-#
-#         state_batch = np.asarray(batch[0], dtype=np.float32)
-#         prob_batch = np.asarray(batch[1],dtype=np.float32)
-#         action_batch = np.asarray(batch[2],dtype=np.int)
-#         next_state_batch = np.asarray(batch[3], dtype=np.float32)
-#         reward_batch = np.asarray(batch[4], dtype=np.float32)
-#         done_batch = np.asarray(batch[5], dtype=np.int32)
-#
-#         return state_batch, prob_batch, action_batch, next_state_batch, reward_batch, done_batch
-
 class Actor(nn.Module):
     def __init__(self,c,action_dim,batch_size):
         super(Actor,self).__init__()
